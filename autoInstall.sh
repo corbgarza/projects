0c156cec43de95670bb2daaae87fd59c1ee3fcbd
corbgarza <garza20@protonmail.com>
2025-03-21 10:49:51 -0500

    update

--- autoInstall.sh
+++ autoInstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install -y brightnessctl build-essential cmake curl feh ffmpeg fonts-noto-color-emoji fzf g++ gettext gpick i3 kitty network-manager ninja-build nm-tray openssh-client openssh-server openvpn pcmanfm picom pkg-config polybar psmisc pulseaudio python3 ripgrep rofi speedtest-cli stow tealdeer vlc x11-xserver-utils xsel zoxide zsh
+sudo apt install -y brightnessctl build-essential cmake curl feh ffmpeg fonts-noto-color-emoji fzf g++ gettext gpick i3 kitty network-manager ninja-build nm-tray openssh-client openssh-server openvpn pcmanfm picom pkg-config polybar psmisc pulseaudio python3 ripgrep rofi samba smbclient speedtest-cli stow tealdeer vlc x11-xserver-utils xsel zoxide zsh
 
 if ! command -v brew > /dev/null; then sudo curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash; fi
 if ! command -v brave-browser > /dev/null; then curl -fsS https://dl.brave.com/install.sh | sh; fi
