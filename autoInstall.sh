3798f99a90bb349253729f95b8f58bbf5c215969
corbgarza <garza20@protonmail.com>
2025-03-23 19:16:21 -0500

    update

--- autoInstall.sh
+++ autoInstall.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-read -p "Install My Files?: " files
-if [ $files = "n" ] || [ $files = "N" ]; then
+read -p "Install My Files?: " FILES
+if [ $FILES = "n" ] || [ $FILES = "N" ]; then
 	echo "SKIPPING"
 else
 	sudo apt install -y brightnessctl btop build-essential cmake curl feh ffmpeg fonts-noto-color-emoji fzf g++ gettext gpick i3 kitty network-manager ninja-build nm-tray openssh-client openssh-server openvpn pcmanfm picom pkg-config polybar psmisc pulseaudio python3 ripgrep rofi samba smbclient speedtest-cli stow tealdeer vlc x11-xserver-utils xsel zoxide zsh
 fi
 
-read -p "Install Homebrew and Brave?: " brewbrave
-if [ $brewbrave = "n" ] || [ $brewbrave = "N" ]; then
+read -p "Install Homebrew and Brave?: " BREWBRAVE
+if [ $BREWBRAVE = "n" ] || [ $BREWBRAVE = "N" ]; then
 	echo "SKIPPING"
 else
 	if ! command -v brew > /dev/null; then sudo curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash; 
@@ -17,8 +17,8 @@ else
 	fi
 fi
 
-read -p "Setup NVIM and dotfiles? " setup
-if [ $setup = "n" ] || [ $setup = "N" ]; then
+read -p "Setup NVIM, dotfiles, fonts, and ZSH? " SETUP
+if [ $SETUP = "n" ] || [ $SETUP = "N" ]; then
 	echo "SKIPPING"
 else
 	git clone https://github.com/corbgarza/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
