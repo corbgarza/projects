2d0900e8635fda9f85c9188b6ba84000afb83c88
corbgarza <garza20@protonmail.com>
2025-07-18 17:01:04 -0500

    update

--- scripts/debauto.sh
+++ scripts/debauto.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-sudo apt install -y nala git >/dev/null && echo "\n****Installed nala and git!****"
-git clone https://github.com/corbgarza/projects.git $HOME/projects >/dev/null && echo "\n****Cloned projects!****"
-git clone https://github.com/corbgarza/dotfiles.git $HOME/dotfiles >/dev/null && echo "\n****Cloned dotfiles!****"
-git clone https://github.com/corbgarza/Wallpaper.git $HOME/Wallpaper >/dev/null && echo "\n****Cloned Wallpaper!****"
+sudo apt install -y curl nala git >/dev/null && echo "****Installed nala and git!****"
+git clone https://github.com/corbgarza/projects.git $HOME/projects >/dev/null && echo "****Cloned projects!****"
+git clone https://github.com/corbgarza/dotfiles.git $HOME/dotfiles >/dev/null && echo "****Cloned dotfiles!****"
+git clone https://github.com/corbgarza/Wallpaper.git $HOME/Wallpaper >/dev/null && echo "****Cloned Wallpaper!****"
 #curl -fsS https://dl.brave.com/install.sh | sh > /dev/null && echo "****Installed Brave!****"
-sudo curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash >/dev/null && echo "\n****Installed Brew!****"
+sudo curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash >/dev/null && echo "****Installed Brew!****"
 
 total=$(wc -l <$HOME/projects/scripts/debauto.progs)
 while IFS=, read -r installer package; do	
@@ -19,12 +19,12 @@ while IFS=, read -r installer package; do
 	esac
 done <$HOME/projects/scripts/debauto.progs
 
-cd $HOME/dotfiles && git remote set-url origin git@github.com:corbgarza/dotfiles.git >/dev/null && echo "\n****Set dotfiles url!****"
-sudo cp $HOME/dotfiles/fonts/* /usr/local/share/fonts/ && echo "\n****Copied Fonts!****"
-sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub && sudo update-grub > /dev/null && echo "\n****Updated Grub!****"
+cd $HOME/dotfiles && git remote set-url origin git@github.com:corbgarza/dotfiles.git >/dev/null && echo "****Set dotfiles url!****"
+sudo cp $HOME/dotfiles/fonts/* /usr/local/share/fonts/ && echo "****Copied Fonts!****"
+sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub && sudo update-grub > /dev/null && echo "****Updated Grub!****"
 #sudo echo "export EDITOR=$(which nvim)" >> /root/.profile && echo "root EDITOR is set to nvim!"
-fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher" && echo '\n****Installed Fisher!****'
+#fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher" && echo '****Installed Fisher!****'
 
 mkdir -p $HOME/.config
 cd $HOME/dotfiles && ls
-echo "\n****Done! Stow dotfiles!****"
+echo "****Done! Stow dotfiles!****"
